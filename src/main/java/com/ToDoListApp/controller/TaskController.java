cca627d56815a3c5e9aa5d888aa1e872f9a1d0a5
bd <203562945+BD-S78@users.noreply.github.com>
2025-04-13 15:47:01 -0400

    m

---
+++ src/main/java/com/ToDoListApp/controller/TaskController.java
@@ -0,0 +1,41 @@
+package com.ToDoListApp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import jakarta.servlet.http.HttpSession;
+import com.ToDoListApp.entity.Task;
+import com.ToDoListApp.entity.User;
+import com.ToDoListApp.repository.TaskRepository;
+import com.ToDoListApp.repository.UserRepository;
+
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+
+
+@Controller
+public class TaskController {
+    @Autowired
+    UserRepository userRepository;
+
+    // posting to different thing without actually being a html thing users can access
+    @PostMapping("/tasks")
+    public String handleTaskSubmission(@RequestParam String title,@RequestParam String description, @RequestParam String priority, @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate duedate, HttpSession session, RedirectAttributes redirectAttributes) {
+        String email = (String) session.getAttribute("email");
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+    	// Save task to database
+        Task task = new Task(title, description);
+        //TaskRepository.save(task);
+
+        // Redirect back to dashboard
+        redirectAttributes.addFlashAttribute("message", "Task added successfully!");
+        String customURL = "/" + user.getFirstName().toLowerCase() + "_" + user.getLastName().toLowerCase() + "/dashboard";
+        return "redirect:" + customURL;
+    }
+}
